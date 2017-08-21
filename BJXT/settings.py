e63f8c1b7a7f3471e2aeadf5cd14352d41723574
mac-yk <164322105@qq.com>
2017-08-21 16:58:55 +0800

    初始xadmin

--- BJXT/settings.py
+++ BJXT/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'xadmin',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class UserProfile(AbstractUser):
     mobile = models.CharField(max_length=11, null=True, blank=True)
-    unit = models.IntegerField(max_length=11, null=True, blank=True)
+    unit = models.IntegerField(null=True, blank=True)
 
     class Meta:
         verbose_name = "用户信息"
